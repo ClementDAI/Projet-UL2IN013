91bcd3ed3e8953493a0b389ee84474bd03b0d8e1
Wonja <tom.coipel@gmail.com>
2026-01-23 18:12:22 +0100

    J'ai corrigé avancer pour robot créer salle, affichage salle

--- Projet.py
+++ Projet.py
@@ -18,33 +18,37 @@ class Robot(object):
 
     def avancer(self,vitesse,distance):
         self.vitesse = vitesse
+        if vitesse == 0:
+            print("Vitesse à 0")
+            return
+        
         t=distance//vitesse
         i=0
         while (i<t):
             if (self.direction == 1):
-                bouger(self,-vitesse,0)
+                self.bouger(0,-vitesse)
             if (self.direction == 2):
-                bouger(self,0,vitesse)
+                self.bouger(vitesse,0)
             if (self.direction == 3):
-                bouger(self,vitesse,0)
+                self.bouger(0,vitesse)
             if (self.direction == 4):
-                bouger(self,0,-vitesse)
+                self.bouger(-vitesse,0)
             i=i+1
             time.sleep(1)
 
-        t=distance%vitesse
+        r=distance%vitesse
         if (self.direction == 1):
-            bouger(self,-t,0)
+            self.bouger(0,-r)
         if (self.direction == 2):
-            bouger(self,0,t)
+            self.bouger(r,0)
         if (self.direction == 3):
-            bouger(self,t,0)
+            self.bouger(0,r)
         if (self.direction == 4):
-            bouger(self,0,-t)
-        time.sleep(t/vitesse)
+            self.bouger(-r,0)
+        time.sleep(r/vitesse)
         self.vitesse = 0
     
-    def Tourner(self,GD): #GD = 0 -> Gauche, GD = 1 -> Droite        
+    def tourner(self,GD): #GD = 0 -> Gauche, GD = 1 -> Droite        
         if (GD == 0):
             if self.direction == 1:
                 self.direction = 4
@@ -56,12 +60,11 @@ class Robot(object):
             else :
                 self.direction += 1
 
-    
 class Salle(object):
     def __init__(self,robot,xmax,ymax):
         self.xmax = xmax
         self.ymax = ymax
-        self.rob = position(robot) #Tuple(x,y)
+        self.rob = robot #on stocke le robot pour pouvoir le voir bouger 
 
     def obstacle(self,x,y):
         self.posx = x
@@ -70,23 +73,38 @@ class Salle(object):
     def cree_matrice(self):
         VIDE = 0
         ROBOT = 1
-        matrice = []
-        liste_y = []
-        for j in range(self.ymax):
-            liste_y.append(VIDE)
-        for i in range(self.xmax):
-            matrice.append(liste_y)
-        x,y = self.rob
-        matrice[x][y] = ROBOT
+        matrice = [[VIDE for _ in range(self.xmax)] for _ in range(self.ymax)]
+        x, y = self.rob.position()
+        matrice[y][x] = ROBOT
         return matrice
 
     def affiche_matrice(self):
         for ligne in self.cree_matrice():
             for case in ligne:
                 if case == 0:
-                    print('_', end(' ')) #si on met un espace on va pas voir les "case" de la matrice juste un R au mileu de rien et je met un espace entre chaque _ pour aéré sinon tout est collé
+                    print('_', end=' ') #si on met un espace on va pas voir les "case" de la matrice juste un R au mileu de rien et je met un espace entre chaque _ pour aérer sinon tout est collé
                 else:
-                    print('R')
+                    print('R', end=' ')
             print()     #retour a la ligne dans l affichage
-                
 
+
+if __name__ == "__main__":
+    r = Robot('R1', 3)
+    s = Salle(r, 5, 5)
+    print("Matrice initiale :")
+    s.affiche_matrice()
+    r.avancer(1,2)
+    print("Matrice Apres avoir avancé de 2 cases vers le bas :")
+    s.affiche_matrice()
+    r.tourner(0)
+    r.avancer(1,2)
+    print("Matrice Apres avoir avancé de 2 cases vers la droite :")
+    s.affiche_matrice()
+    r.tourner(0)
+    r.avancer(1,2)
+    print("Matrice Apres avoir avancé de 2 cases vers le haut :")
+    s.affiche_matrice()
+    r.tourner(0)
+    r.avancer(1,2)
+    print("Matrice Apres avoir avancé de 2 cases vers la gauche :")
+    s.affiche_matrice()
