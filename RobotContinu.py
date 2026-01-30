4cc00cc02f2ff11a90dbb8c92c717711224db23d
Frixel-w <wangmartial0@gmail.com>
2026-01-30 20:39:04 +0100

    ajout de l affichage selon l orientation
    la fleche rouge quoi

--- RobotContinu.py
+++ RobotContinu.py
@@ -9,7 +9,6 @@ while True:
         vitesse = int(vitesse)
         break
 
-
 pygame.init()
 screen = pygame.display.set_mode((1440, 810))  # Taille de la fenetre largeur x hauteur
 pygame.display.set_caption("Simulation Dexter") #Titre de la fenetre
@@ -35,7 +34,9 @@ class Robot(object):
         if (self.angle == 270):
             self.pos.x -= self.speed * dt
 
-r = Robot("r2d2",vitesse,screen.get_width() / 2,screen.get_height() / 2) #speed temporairement a 50, il faudra qu il soit ajustable sans toucher le code avec x y correspond au centre de la fenetre comme dit avant
+r = Robot("r2d2",vitesse,screen.get_width() / 2,screen.get_height() / 2) # x et y correspond a sa position de départ, ici c est le centre de la fenetre
+l1 = r.largeur
+l2 = r.longueur
 
 while running:
     for event in pygame.event.get():
@@ -44,25 +45,49 @@ while running:
 
     screen.fill("white") #couleur de la fenetre
 
-    pygame.draw.rect(screen, "black", pygame.Rect(r.pos.x - r.largeur/2, r.pos.y - r.longueur/2, r.largeur, r.longueur)) #70 pixels de haut 40 pixel de large et centré sur la position de base
+    pygame.draw.rect(screen, "black", pygame.Rect(r.pos.x - l1/2, r.pos.y - l2/2, l1, l2)) #70 pixels de haut 40 pixel de large et centré sur la position de base
 
-    pygame.draw.line(screen, "red", r.pos + pygame.Vector2(0, -35), r.pos + pygame.Vector2(0, -50), 2) #ligne pour la direction
-    pygame.draw.line(screen, "red", r.pos + pygame.Vector2(0, -50), r.pos + pygame.Vector2(-5, -45), 2) #coté gauche de la fleche
-    pygame.draw.line(screen, "red", r.pos + pygame.Vector2(0, -50), r.pos + pygame.Vector2(5, -45), 2) #coté droit de la fleche
+    if (r.angle == 0):
+        pygame.draw.line(screen, "red", r.pos + pygame.Vector2(0, -r.longueur/2), r.pos + pygame.Vector2(0, -r.longueur/2-15), 2) #ligne pour la direction, 15 la longueur de la fleche
+        pygame.draw.line(screen, "red", r.pos + pygame.Vector2(0, -r.longueur/2-15), r.pos + pygame.Vector2(-5, -r.longueur/2-10), 2) #coté gauche de la fleche, -5 et +5 pour faire un ligne diagonale entre le bout de la ligne et le bout du coté
+        pygame.draw.line(screen, "red", r.pos + pygame.Vector2(0, -r.longueur/2-15), r.pos + pygame.Vector2(5, -r.longueur/2-10), 2) #coté droit de la fleche
+    
+    if (r.angle == 90):
+        pygame.draw.line(screen, "red", r.pos + pygame.Vector2(r.longueur/2, 0), r.pos + pygame.Vector2(r.longueur/2+15, 0), 2) 
+        pygame.draw.line(screen, "red", r.pos + pygame.Vector2(r.longueur/2+15, 0), r.pos + pygame.Vector2(r.longueur/2+10, 5), 2)
+        pygame.draw.line(screen, "red", r.pos + pygame.Vector2(r.longueur/2+15, 0), r.pos + pygame.Vector2(r.longueur/2+10, -5), 2)
+
+    if (r.angle == 180):
+        pygame.draw.line(screen, "red", r.pos + pygame.Vector2(0, r.longueur/2), r.pos + pygame.Vector2(0, r.longueur/2+15), 2) 
+        pygame.draw.line(screen, "red", r.pos + pygame.Vector2(0, r.longueur/2+15), r.pos + pygame.Vector2(5, r.longueur/2+10), 2)
+        pygame.draw.line(screen, "red", r.pos + pygame.Vector2(0, r.longueur/2+15), r.pos + pygame.Vector2(-5, r.longueur/2+10), 2)
+
+    if (r.angle == 270):
+        pygame.draw.line(screen, "red", r.pos + pygame.Vector2(-r.longueur/2, 0), r.pos + pygame.Vector2(-r.longueur/2-15, 0), 2) 
+        pygame.draw.line(screen, "red", r.pos + pygame.Vector2(-r.longueur/2-15, 0), r.pos + pygame.Vector2(-r.longueur/2-10, -5), 2) 
+        pygame.draw.line(screen, "red", r.pos + pygame.Vector2(-r.longueur/2-15, 0), r.pos + pygame.Vector2(-r.longueur/2-10, 5), 2) 
     
     keys = pygame.key.get_pressed()
     if keys[pygame.K_z]:
         r.angle = 0
         r.avancer_reculer(dt)
+        l1 = r.largeur
+        l2 = r.longueur
     if keys[pygame.K_s]:
         r.angle = 180
         r.avancer_reculer(dt)
+        l1 = r.largeur
+        l2 = r.longueur
     if keys[pygame.K_q]:
         r.angle = 270
         r.avancer_reculer(dt)
+        l1 = r.longueur
+        l2 = r.largeur
     if keys[pygame.K_d]:
         r.angle = 90
         r.avancer_reculer(dt)
+        l1 = r.longueur
+        l2 = r.largeur
 
     pygame.display.flip() #met a jour l'ecran
 
