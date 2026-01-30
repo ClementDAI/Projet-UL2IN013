8f762d5ecfb8dd977de36aca958dc6d6e4db0302
Frixel-w <wangmartial0@gmail.com>
2026-01-30 01:24:43 +0100

    ajout de la classe robot
    et modification dans affichage pour que sa utilisse la class robot et ses valeur

--- RobotContinu.py
+++ RobotContinu.py
@@ -10,7 +10,7 @@ class Robot(object):
     def __init__(self,nom,speed,x,y):
         self.nom = nom
         self.pos = pygame.Vector2(x,y) #position du robot , avec pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2) le robot commence au toujour au centre de la fenetre
-        self.angle = 0 # l angle 0 va correspondre a vers le haut
+        self.angle = 0 # l'angle 0 va correspondre a vers le haut et pour rappel dans pygame x est vers la droite et y vers le bas comme pour la matrice qu on a fait
         self.speed = speed
         self.largeur = 40
         self.longueur = 70
@@ -19,7 +19,7 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: #fermer la fenetre si on appuie sur la croix
             running = False
-    r = Robot("r2d2",0,screen.get_width() / 2,screen.get_height() / 2)
+    r = Robot("r2d2",0,screen.get_width() / 2,screen.get_height() / 2) #speed temporairement 0 car pas encore codé avec x y correspond au centre de la fenetre comme dit avant
     screen.fill("white") #couleur de la fenetre
 
     pygame.draw.rect(screen, "black", pygame.Rect(r.pos.x - r.largeur/2, r.pos.y - r.longueur/2, r.largeur, r.longueur)) #70 pixels de haut 40 pixel de large et centré sur la position de base
