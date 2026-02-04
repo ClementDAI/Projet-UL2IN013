bdd56bd5310e0263ffe31f8f5e043bf8d9b1ed49
Martial WANG <wangmartial0@gmail.com>
2026-02-04 19:13:14 +0100

    ajout de la classe Robot avec l initialisation de l objet et une courte fonction pour renvoyer ses coordonées x y au besoin

--- ClassRobot.py
+++ ClassRobot.py
@@ -1,10 +1,12 @@
 class robot(object):
 
-    def __init__(self,x,y,vitesse,angle):
+    def __init__(self,x,y,vitesse,angle,longueur,largeur):
         self.x = x #coordoné x du centre du robot
         self.y = y #coordoné y du centre du robot
         self.vitesse = vitesse #vitesse en pixel par seconde
         self.angle = angle #angle en degrés de son orientation avec l'angle a 0
+        self.longueur = longueur #valeur de sa longueur sur y
+        self.largeur = largeur #valeur de sa largeur sur x
     
     def getPostion(self):
         return self.x,self.y #renvoie le x et y du robot
