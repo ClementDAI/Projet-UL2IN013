9bcb389b36f99927228ce648f19bc159af4dac1b
Martial WANG <wangmartial0@gmail.com>
2026-02-04 19:26:34 +0100

    ajout de la classe Obstacle avec l initialisation de l objet

--- ClassObstacle.py
+++ ClassObstacle.py
@@ -5,9 +5,3 @@ class obstacle(objet):
         self.y = y #coordoné y du centre du centre de l obstacle
         self.longueur = longueur #valeur de sa longueur sur y
         self.largeur = largeur #valeur de sa largeur sur x
-    
-    def getMoitié(self,cote): #renvoit la distance qu il y a entre son centre et son coté
-        if cote == "gauche" or cote == "droite":
-            return self.largeur/2
-        if cote == "haut" or cote == "bas":
-            return self.longueur/2
