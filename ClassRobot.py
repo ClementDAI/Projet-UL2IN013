ea8876f3b5dc16879102fd8c8a4d21dfc5adfcaf
clement dai <Clement.Dai@etu-sorbonne.universite.fr>
2026-02-07 18:07:00 +0100

    Fonction 'rotation' qui fait tourner dans la direction de la cible et Fonction 'tourner' qui fait tourner selon un angle

--- ClassRobot.py
+++ ClassRobot.py
@@ -15,26 +15,13 @@ class robot(object):
     def getPosition(self):
         return self.x,self.y #renvoie le x et y du robot
     
-    def tourner(self, x_cible, y_cible):
-        """Fait tourner le robot vers une direction cible (x_cible,y_cible)"""
-        xVecteur1,yVecteur1 = (x_cible-self.x, y_cible-self.y) #Vecteur vers la direction cible
-
-        #Calcul de x et y du vecteur qui a pour direction self.angle 
-        #On considère la norme du vecteur de la direction du robot à 1
-        #Attention sur repère orthonormé 
-        if self.angle >= 0 and self.angle <=90:
-            xVecteur2, yVecteur2 = (math.cos(90-self.angle) * 1, math.cos(self.angle) *1),   #Trigonométrie
-        elif self.angle >= 90 and self.angle <=180:
-            xVecteur2, yVecteur2 = (math.cos(self.angle-90) * 1, -math.cos(180-self.angle) *1)
-        elif self.angle >= 180 and self.angle <=270:
-            xVecteur2, yVecteur2 = (-math.cos(270-self.angle) * 1, -math.cos(self.angle-180) *1)
-        elif self.angle >= 270 and self.angle <=360:
-            xVecteur2, yVecteur2 = (-math.cos(self.angle-270) * 1, cos(360-self.angle) *1)
-
-
-        prScalaire = xVecteur1*xVecteur2 + yVecteur1*yVecteur2
-        theta = math.acos(prScalaire/1*math.sqrt(xVecteur1**2+yVecteur1**2)%360
-        self.angle+=theta
+    def tourner(self,angle):
+        """Fait tourner le robot de {angle}° """
+        self.angle += angle
+    
+    def rotation (self, x_cible, y_cible):
+        """Fait une rotation du robot vers une direction cible (x_cible,y_cible)"""
+        self.angle = (math.degrees(math.atan2(y_cible-self.y,x_cible-self.x)) + 90)% 360
     
     def avancer(self):
         """
@@ -54,10 +41,7 @@ class robot(object):
         """
         distance = math.sqrt((x - self.x)**2 + (y - self.y)**2) #pythagore
         while distance > 1: #pas egalité a cause des floats et arrondis
-            anglerad = math.atan2(y - self.y, x - self.x) #angle en radiant entre position actuelle et position cible
-            angle2 = math.degrees(anglerad) + 90  # +90 parce que l'angle 0 c'est vers le haut et pas vers la droite
-            angle2 = angle2 % 360
-            self.angle = angle2 #on utilise pas tourner parce quon tourne qu'une seule fois vers la cible pas progressivement
+            self.rotation(x,y)
             self.avancer()
             distance = math.sqrt((x - self.x)**2 + (y - self.y)**2)  # Recalculer la distance
         self.x = round(x, 2) #pour eviter que le robot soit a 0.99/1.01 de la cible a cause des floats
