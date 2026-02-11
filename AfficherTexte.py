8e2b68ad51af943d22ca302d9c842bb308583390
Clement Dai <Clement.Dai@etu-sorbonne.universite.fr>
2026-02-11 09:25:45 +0100

    Correction de bugs

--- AfficherTexte.py
+++ AfficherTexte.py
@@ -8,8 +8,8 @@ petite_police= pygame.font.Font("mytype.ttf",24)
 
 commande_c=petite_police.render("Commande C en cours d'utilisation", True, Bleu_saphir)
 commande_a=petite_police.render("Commande A en cours d'utilisation", True, Bleu_saphir)
-texte_controleC=petite_police.render("touche C : Faire un carré", True, Bleu_saphir)
-texte_controleA=petite_police.render("touche A : Aller vers des coordonnées à entrer", True, Bleu_saphir)
+texte_controleC=petite_police.render("touche C : Faire un carre", True, Bleu_saphir)
+texte_controleA=petite_police.render("touche A : Aller vers des coordonnees a entrer", True, Bleu_saphir)
 
 def afficher_le_texte(surface_cible, touche_pressee):
     if touche_pressee=="menu" or touche_pressee is None:
--- ClassRobot.py
+++ ClassRobot.py
@@ -45,91 +45,21 @@ class robot(object):
         fait avancer le robot de 1 pixel dans la direction de son angle
         aucun paramètre
         """
-        self.x += 1 * math.sin(math.radians(self.angle))
-        self.y -= 1 * math.cos(math.radians(self.angle))
+        self.x += 0.1 * math.sin(math.radians(self.angle))
+        self.y -= 0.1 * math.cos(math.radians(self.angle))
         self.x = round(self.x, 2) #arrondi pour eviter les problemes de précision avec les floats
         self.y = round(self.y, 2)
 
-    def collision(self, salle):
-        """
-        salle : paramètre de classe Salle
-        Collision va renvoyer true si la position du robot est bloqué par un obsctacle de la salle sinon false
-        """
-
-        angle = np.deg2rad(self.angle - 90)
-        cos = np.cos(angle)
-        sin = np.sin(angle)
-        larg = self.largeur / 2
-        long = self.longueur / 2
-        
-        coinHG = np.array([(self.x - (larg * cos) - (long * sin)), (self.y - (larg * sin) + (long * cos))]) # calcul des coins du robots
-        coinHD = np.array([(self.x + (larg * cos) - (long * sin)), (self.y + (larg * sin) + (long * cos))]) 
-        coinBG = np.array([(self.x - (larg * cos) + (long * sin)), (self.y - (larg * sin) - (long * cos))])
-        coinBD = np.array([(self.x + (larg * cos) + (long * sin)), (self.y + (larg * sin) - (long * cos))])
-
-        cote_robot = [(coinHG, coinHD), (coinBG, coinBD), (coinBG, coinHG), (coinBD, coinHD)]
-
-        for i in salle.ListeObstacle:
-            xObstacle, yObstacle = i.x, i.y
-            largeurObstacle = i.largeur
-            longueurObstacle = i.longueur
-            coinObsHG = np.array([(xObstacle - (largeurObstacle / 2)), (yObstacle + (longueurObstacle / 2))]) # calculs des coins de l'obstacle
-            coinObsHD = np.array([(xObstacle + (largeurObstacle / 2)), (yObstacle + (longueurObstacle / 2))])
-            coinObsBG = np.array([(xObstacle - (largeurObstacle/ 2)), (yObstacle - (longueurObstacle / 2))])
-            coinObsBD = np.array([(xObstacle + (largeurObstacle / 2)), (yObstacle - (longueurObstacle / 2))])
-            cote_obs = [(coinObsHG, coinObsHD), (coinObsBG, coinObsBD), (coinObsBG, coinObsHG), (coinObsBD, coinObsHD)]
-            for point1_rob, point2_rob in cote_robot:
-                for point1_obs, point2_obs in cote_obs: 
-                    vect_rob = point2_rob - point1_rob
-                    vect_obs = point2_obs - point1_obs
-
-                    prod_vec1 = np.cross(vect_rob, point1_obs - point1_rob) # calcul des produits vectoriels entre les cotés du robots et celle de l'obstacle pour détecter la collision
-                    prod_vec2 = np.cross(vect_rob, point2_obs - point1_rob)
-                    prod_vec3 = np.cross(vect_obs, point1_rob - point1_obs)
-                    prod_vec4 = np.cross(vect_obs, point2_rob - point1_obs)
-
-                    if (prod_vec1 * prod_vec2) < 0 and (prod_vec3 * prod_vec4) < 0: 
-                        return True
-                    
-        cote_salle = [(np.array([0, 0]), np.array([salle.dimensionX, 0])), (np.array([0, salle.dimensionY]), np.array([salle.dimensionX, salle.dimensionY])), (np.array([0, salle.dimensionY]), np.array([0, 0])), (np.array([salle.dimensionX, salle.dimensionY]), np.array([salle.dimensionX, 0]))]
-        
-        for point1_rob, point2_rob in cote_robot: # collision avec la salle
-            for point1_salle, point2_salle in cote_salle: 
-                vect_rob = point2_rob - point1_rob
-                vect_salle = point2_salle - point1_salle
-
-                prod_vec1 = np.cross(vect_rob, point1_salle - point1_rob)
-                prod_vec2 = np.cross(vect_rob, point2_salle - point1_rob)
-                prod_vec3 = np.cross(vect_salle, point1_rob - point1_salle)
-                prod_vec4 = np.cross(vect_salle, point2_rob - point1_salle)
-
-                if (prod_vec1 * prod_vec2) < 0 and (prod_vec3 * prod_vec4) < 0: 
-                    return True
-        
-        return False
-
-    
-    def aller_a(self, salle, x, y):
+    def aller_a(self, x, y):
         """
         fonction aller_a
         Si il n'y a pas d'obstacles : fait avancer le robot jusqu'à la position (x,y) passée en paramètre 
         Sinon : arrête le robot
         """
         distance = math.sqrt((x - self.x)**2 + (y - self.y)**2) #pythagore
-        xtmp = self.x
-        ytmp = self.y
-        while distance > 1: #pas egalité a cause des floats et arrondis
+        while distance > 0.1: #pas egalité a cause des floats et arrondis
             self.rotation(x,y)
-            xtmp += 1 * math.sin(math.radians(self.angle))
-            ytmp -= 1 * math.cos(math.radians(self.angle))
-            xtmp = round(xtmp, 2)
-            ytmp = round(ytmp, 2)
-            tmp = robot(xtmp, ytmp, self.vitesse, self.angle, self.longueur, self.largeur) #Création d'un robot temporaire qui effectue un pas en 'avance'
-            if tmp.collision(salle) == True:
-                print("Il y a un obstacle sur le chemin")
-                break
-            else:
-                self.avancer()
+            self.avancer()
             distance = math.sqrt((x - self.x)**2 + (y - self.y)**2)  # Recalculer la distance
         self.x = round(x, 2) #pour eviter que le robot soit a 0.99/1.01 de la cible a cause des floats
         self.y = round(y, 2)
