ee3fc932526d0e4be70858ef72b203ace46be400
Clement Dai <Clement.Dai@etu-sorbonne.universite.fr>
2026-02-14 13:01:06 +0100

    Ajout de l'attribut inclinaison d'un obstacle

--- obstacle.py
+++ obstacle.py
@@ -1,7 +1,8 @@
 class Obstacle():
 
-    def __init__(self,x,y,largeur,longueur): #obstacle de forme rectangulaire centré x,y
-        self.x = x #coordoné x du centre du centre de l obstacle
-        self.y = y #coordoné y du centre du centre de l obstacle
+    def __init__(self,x,y,largeur,longueur,inclinaison): #obstacle de forme rectangulaire centré x,y
+        self.x = x #coordonnée x du centre du centre de l obstacle
+        self.y = y #coordonnée y du centre du centre de l obstacle
         self.longueur = longueur #valeur de sa longueur sur y
         self.largeur = largeur #valeur de sa largeur sur x
+        self.inclinaison = inclinaison #obstacle incliné de l'angle {inclinaison} en degré
