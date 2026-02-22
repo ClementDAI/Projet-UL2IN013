5da3d7009a316e339453bebda070779e6559e90a
Wonja <tom.coipel@gmail.com>
2026-02-22 17:17:08 +0100

    D'autres fichiers nécéssaires

---
+++ Semaine5/Controller.py
@@ -0,0 +1,6 @@
+from ClassRobot import robot
+import math
+#But : Regarde l’état du monde (output de simulation) et renvoie les actions à gérer (à robot et à simulation)
+#def rotation(robot, x_cible, y_cible):
+#def avancer(robot):
+#def aller_a(robot, x, y):
