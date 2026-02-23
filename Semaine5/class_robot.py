0d4f5af790a98ac943a8a6a2ea75dbe605563062
Martial WANG <wangmartial0@gmail.com>
2026-02-23 15:01:09 +0100

    ajout des fonction et des getter setter necesaire pour le controller

---
+++ Semaine5/class_robot.py
@@ -0,0 +1,84 @@
+import math
+import numpy as np
+
+class robot(object):
+
+    def __init__(self,x,y,vangGauche,vangDroite,angle,longueur,largeur):
+        self.x = x #coordonné x du centre du robot
+        self.y = y #coordonné y du centre du robot
+        self.angle = angle #angle positif ou négatif en degré dont son orientation initial est 0 (vers le haut)
+        self.longueur = longueur #valeur de sa longueur sur y
+        self.largeur = largeur #valeur de sa largeur sur x
+        self.vangGauche = vangGauche #vitesse angulaire de la roue gauche en rad/s
+        self.vangDroite = vangDroite #vitesse angulaire de la roue droite en rad/s
+        self.rayonRoues = 0.05 #rayon des roues en m
+        self.ecartRoues = 0.2 #écart entre les roues en m
+        self.capteur = 0 #distance entre le robot et l obstacle en face (ou la bordure de la salle)
+        self.vitesseLineaire
+        self.vitesseAngulaire
+
+    def coins(self):
+        angle = np.deg2rad(self.angle)
+        cos = np.cos(angle)
+        sin = np.sin(angle)
+        larg = self.largeur / 2
+        long = self.longueur / 2
+        
+        coinHG = np.array([(self.x - (larg * cos) - (long * sin)), (self.y - (larg * sin) + (long * cos))]) # calcul des coins du robots
+        coinHD = np.array([(self.x + (larg * cos) - (long * sin)), (self.y + (larg * sin) + (long * cos))]) 
+        coinBG = np.array([(self.x - (larg * cos) + (long * sin)), (self.y - (larg * sin) - (long * cos))])
+        coinBD = np.array([(self.x + (larg * cos) + (long * sin)), (self.y + (larg * sin) - (long * cos))])
+
+        return [(coinHG, coinHD), (coinBG, coinBD), (coinBG, coinHG), (coinBD, coinHD)]
+    
+    def calculerVitesses(self):
+        """
+        Calcule la vitesse linéaire et la vitesse angulaire du robot à partir des vitesses angulaires des roues
+        Rappel : la vitesse angulaire du robot est la vitesse à laquelle il tourne sur lui même,
+        la vitesse linéaire est la vitesse à laquelle il avance dans la direction de son angle
+        """
+        # Vitesses linéaires des roues
+        v_gauche = self.rayonRoues * self.vangGauche
+        v_droite = self.rayonRoues * self.vangDroite
+        
+        # Vitesse linéaire du robot (moyenne des deux roues)
+        self.vitesseLineaire = (v_gauche + v_droite) / 2
+        
+        # Vitesse angulaire du robot (rotation)
+        self.vitesseAngulaire = (v_droite - v_gauche) / self.ecartRoues
+    
+    def getPosition(self):
+        return self.x,self.y #renvoie le x et y du robot
+    
+    def setVitessesAngulaires(self, vangGauche, vangDroite):
+        """
+        Setter pour les vitesses angulaires des roues
+        vangGauche = vitesse angulaire de la roue gauche en rad/s
+        vangDroite = vitesse angulaire de la roue droite en rad/s
+        """
+        self.vangGauche = vangGauche
+        self.vangDroite = vangDroite
+        self.calculerVitesses()
+    
+    def getVitessesAngulaires(self):
+        """
+        Getter pour les vitesses angulaires des roues
+        Retourne: (vangGauche, vangDroite)
+        """
+        return self.vangGauche, self.vangDroite
+    
+    def getVitesses(self):
+        """
+        Getter pour les vitesses linéaire et angulaire du robot
+        Retourne: (vitesseLineaire, vitesseAngulaire)
+        """
+        return self.vitesseLineaire, self.vitesseAngulaire
+    
+    def update_angle(self,angle):
+        """Fait tourner le robot de {angle}° """
+        self.angle = angle
+
+    def normaliser_angle(self):
+        """Normalise self.angle dans l'intervalle [-180, 180]."""
+        self.angle = (self.angle + 180) % 360 - 180
+    
