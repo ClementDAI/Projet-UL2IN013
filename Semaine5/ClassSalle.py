8ef96b0cc018c1bdb7112cab6305d6f747c04aa1
Wonja <tom.coipel@gmail.com>
2026-02-23 10:15:06 +0100

    Classe salle

---
+++ Semaine5/ClassSalle.py
@@ -0,0 +1,5 @@
+class salle(object):
+
+    def __init__(self,longueur,largeur):
+        self.dimensionX = longueur #j'ai mit dimensionX en nom d argument pcq robot et obstacle s appelle deja comme sa pour pas trop ce répéter
+        self.dimensionY = largeur #j'ai mit dimensionY en nom d argument pcq robot et obstacle s appelle deja comme sa pour pas trop ce répéter
