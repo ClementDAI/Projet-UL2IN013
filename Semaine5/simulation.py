36cec3f59235bb4b344e82cf0cc15ae0240f0f44
Beyza76 <s33947870@gmail.com>
2026-02-25 00:10:34 +0100

    Update simulation.py

--- Semaine5/simulation.py
+++ Semaine5/simulation.py
@@ -59,7 +59,7 @@ class Simulation:
     
         return False
     
-    def updateSimulation(self,Robot):
+    def update_simulation(self,Robot):
         """
         updateSimulation va mettre à jour la position du robot en fonction de sa vitesse et de son angle d'orientation
         """
