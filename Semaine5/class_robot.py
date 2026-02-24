9551ce83d448c663f5c8e891caf818f741ab6c1a
Beyza76 <s33947870@gmail.com>
2026-02-24 23:27:24 +0100

    Update class_robot.py

--- Semaine5/class_robot.py
+++ Semaine5/class_robot.py
@@ -5,10 +5,10 @@ class Robot(object):
 
     def __init__(self,x,y,vangGauche,vangDroite,angle,longueur,largeur):
         self.x = x #coordonné x(abscisse) du centre du robot
-        self.y = y #coordonné y du centre du robot
+        self.y = y #coordonné y (ordonnée) du centre du robot
         self.angle = angle #angle positif ou négatif en degré dont son orientation initial est 0 (vers le haut)
-        self.longueur = longueur #valeur de sa longueur sur y
-        self.largeur = largeur #valeur de sa largeur sur x
+        self.longueur = longueur #valeur de la longueur sur y du robot
+        self.largeur = largeur #valeur de la  largeur sur x du robot
         self.vangGauche = vangGauche #vitesse angulaire de la roue gauche en rad/s
         self.vangDroite = vangDroite #vitesse angulaire de la roue droite en rad/s
         self.rayonRoues = 0.05 #rayon des roues en m
