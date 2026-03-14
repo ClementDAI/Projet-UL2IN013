daacec004bcaa0d243ea49d227ff547e1813913a
Mattéo Zhou <matteo.zhou@etu.sorbonne-universite.fr>
2026-03-14 14:47:12 +0100

    ajout de la stratégie avancer

---
+++ Semaine5/avancer.py
@@ -0,0 +1,20 @@
+from robot import Robot
+import math
+
+class Avancer:
+    def __init__(self, distance, rob):
+        self.distance = distance
+        self.rob = rob
+        
+    def start(self):
+        self.parcouru = 0
+
+    def step(self):
+        self.rob.calculerVitesses()
+        distance = self.rob.vitesseLineaire * 0.1
+        self.rob.x += distance * math.sin(math.radians(self.rob.angle))
+        self.rob.y -= distance * math.cos(math.radians(self.rob.angle))
+        self.parcouru += distance
+
+    def stop(self):
+        return self.parcouru > self.distance
