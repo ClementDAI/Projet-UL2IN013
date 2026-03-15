fd7067424428d2f1158496f07c2e2f799adb9e29
Mattéo Zhou <matteo.zhou@etu.sorbonne-universite.fr>
2026-03-15 19:07:41 +0100

    ajout carre.py à vérifier après tourner.py

---
+++ CurrentCode/carre.py
@@ -0,0 +1,24 @@
+from avancer import Avancer
+from tourner import Tourner
+
+class Carre:
+    def __init__(self, cote, rob):
+        self.rob = rob
+        self.cote = cote
+        self.strats = [Avancer(self.cote, self.rob), Tourner(90, self.rob), Avancer(self.cote, self.rob), Tourner(90, self.rob), Avancer(self.cote, self.rob), Tourner(90, self.rob), Avancer(self.cote, self.rob)]
+        self.cur = -1
+
+    def start(self):
+        self.cur = -1
+
+    def start(self):
+        if self.stop():
+            return
+        
+        if self.cur < 0 or self.strats[self.cur].stop():
+            self.cur += 1
+            self.strats[self.cur].start()
+        self.strats[self.cur].step()
+
+    def stop(self):
+        return self.cur == len(self.strats) - 1 and self.strats[self.cur].stop()
