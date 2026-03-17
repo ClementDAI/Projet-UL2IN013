78cf5077723477838f79e304073a8000468c48c3
Mattéo Zhou <matteo.zhou@etu.sorbonne-universite.fr>
2026-03-18 00:06:32 +0100

    correction de controller pour qu'il s'occupe de demander l'action à exécuter et le faire fonctionner et s'arreter

--- CurrentCode/controller.py
+++ CurrentCode/controller.py
@@ -1,23 +1,42 @@
-from robot import Robot
 from carre import Carre
 from avancer import Avancer
 from tourner import Tourner
-from boucle import Boucle
-#normalement ici tout marche mais cf boucle.py
+from approcher_mur import Approcher_mur
+
 class Controller:
-    def __init__(self, Robot):
-        self.robot = Robot
-        self.xprec = Robot.x
-        self.yprec = Robot.y
-        self.distance_parcourue = 0
+    def __init__(self, robot):
+        self.robot = robot
+        self.strat = ""
         self.action = None
     
-    def step(self): 
-        """Correspond à updateController """
-        self.action = input("Entrez une action : 0 pour Avancer, 1 pour tourner, 2 pour carré, 3 pour s'approcher d'un mur")
-    
-
-
-
-
+    def update(self): 
+        """met à jour le controller """
+        if self.action == None:
+            self.strat = input("Entrez une action : 0 pour Avancer, 1 pour tourner, 2 pour carré, 3 pour s'approcher d'un mur : ")
+            if self.strat == "0":
+                valeur = float(input("Distance (positif) que le robot avance : "))
+                self.action = Avancer(valeur, self.robot)
+                self.action.start()
+            
+            elif self.strat == "1":
+                valeur = float(input("Angle (positif) que le robot doit tourner : "))
+                self.action = Tourner(valeur, self.robot)
+                self.action.start()
+            
+            elif self.strat == "2":
+                valeur = float(input("Valeur (positif) du coté du carré à tracer par le robot : "))
+                self.action = Carre(valeur, self.robot)
+                self.action.start()
+            
+            elif self.strat == "3":
+                self.action = Approcher_mur(self.robot)
+                self.action.start()
+            
+            else:
+                print("Choix invalide, aucune action, veuillez entrez une action valide : 0, 1, 2, 3")
+        else:
+            if not self.action.stop():
+                self.action.step() 
+            else:
+                self.action = None
 
