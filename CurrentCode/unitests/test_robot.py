086937070751280f880488de78c282e56893e5e7
Wonja <tom.coipel@gmail.com>
2026-03-19 17:10:35 +0100

    test robot et salle avec la bonne bibliotheque unitest

--- CurrentCode/unitests/test_robot.py
+++ CurrentCode/unitests/test_robot.py
@@ -1,44 +1,29 @@
 from R2D2.simulation.robot import Robot
-def test_coins():
-    robot = Robot(0, 0, 0, 0, 0, 0.2, 0.1) #robot orienté vers le haut
-    coins = robot.coins()
-    assert coins[0][0][0] == -0.05 and coins[0][0][1] == 0.1 #coin haut-gauche
-    assert coins[0][1][0] == 0.05 and coins[0][1][1] == 0.1 #coin haut-droit
-    assert coins[1][0][0] == -0.05 and coins[1][0][1] == -0.1 #coin bas-gauche
-    assert coins[1][1][0] == 0.05 and coins[1][1][1] == -0.1 #coin bas-droit
+import unittest
 
-def test_calculerVitesses():
-    robot = Robot(0, 0, 1, 1, 0, 0.3, 0.2) #ligne droite
-    vlin, vang = robot.calculerVitesses()
-    assert vlin == 0.05
-    assert vang == 0
+class TestRobot(unittest.TestCase):
+    def setUp(self):
+        self.robot = Robot(0, 0, 0, 0, 0, 0.2, 0.1)
 
-    robot2 = Robot(0, 0, -1, 1, 0, 0.3, 0.2) #roues opposées
-    robot2.calculerVitesses()
-    assert robot2.vitesseLineaire == 0
-    assert robot2.vitesseAngulaire > 0
+    def test_coins(self):
+        coins = self.robot.coins()
+        self.assertEqual(coins[0][0][0], -0.05)
+        self.assertEqual(coins[0][0][1], 0.1)
+        self.assertEqual(coins[0][1][0], 0.05)
+        self.assertEqual(coins[0][1][1], 0.1)
+        self.assertEqual(coins[1][0][0], -0.05)
+        self.assertEqual(coins[1][0][1], -0.1)
+        self.assertEqual(coins[1][1][0], 0.05)
+        self.assertEqual(coins[1][1][1], -0.1)
 
-def test_update_angle():
-    robot = Robot(0, 0, 0, 0, 0, 0.2, 0.1)
-    robot.update_angle(90)
-    assert robot.angle == 90
-    robot.update_angle(-90)
-    assert robot.angle == -90
+    def test_calculerVitesses(self):
+        robot = Robot(0, 0, 1, 1, 0, 0.3, 0.2) #ligne droite
+        vlin, vang = robot.calculerVitesses()
+        self.assertEqual(vlin, 0.05)
+        self.assertEqual(vang, 0)
 
-def test_normaliser_angle():
-    robot = Robot(0, 0, 0, 0, 0, 0.2, 0.1)
-    robot.update_angle(270)
-    robot.normaliser_angle()
-    assert robot.angle == -90
-    robot.update_angle(-270)
-    robot.normaliser_angle()
-    assert robot.angle == 90
-
-def test_assurer_position_avant():
-    robot = Robot(0, 0, 0, 0, 0, 0.2, 0.1)
-    robot.update_angle(90)
-    robot.assurer_direction_avant()
-    assert robot.angle == 90
-    robot.update_angle(-90)
-    robot.assurer_direction_avant()
-    assert robot.angle == -90
+    def test_calculerVitesses_roues_opposees(self):
+        robot2 = Robot(0, 0, -1, 1, 0, 0.3, 0.2) #roues opposées
+        robot2.calculerVitesses()
+        self.assertEqual(robot2.vitesseLineaire, 0)
+        self.assertGreater(robot2.vitesseAngulaire, 0)
--- CurrentCode/unitests/test_salle.py
+++ CurrentCode/unitests/test_salle.py
@@ -1,8 +1,18 @@
 from R2D2.simulation.salle import Salle
-def test_salle():
-    salle = Salle(10, 5)
-    coins = salle.coins()
-    assert coins[0][0][0] == 0 and coins[0][0][1] == 0 #coin bas-gauche
-    assert coins[0][1][0] == 10 and coins[0][1][1] == 0 #coin bas-droit
-    assert coins[1][0][0] == 0 and coins[1][0][1] == 5 #coin haut-gauche
-    assert coins[1][1][0] == 10 and coins[1][1][1] == 5 #coin haut-droit
+import unittest
+    
+class TestSalle(unittest.TestCase):
+    def test_salle(self):
+        salle = Salle(10, 5)
+        coins = salle.coins()
+        self.assertEqual(coins[0][0][0], 0)
+        self.assertEqual(coins[0][0][1], 0)
+        self.assertEqual(coins[0][1][0], 10)
+        self.assertEqual(coins[0][1][1], 0)
+        self.assertEqual(coins[1][0][0], 0)
+        self.assertEqual(coins[1][0][1], 5)
+        self.assertEqual(coins[1][1][0], 10)
+        self.assertEqual(coins[1][1][1], 5)
+    
+if __name__ == '__main__':
+    unittest.main()
