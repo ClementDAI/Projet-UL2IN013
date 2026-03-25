8d8453496799729e399d5b7112546eeb143e4d3f
Martial WANG <wangmartial0@gmail.com>
2026-03-25 09:13:31 +0100

    fix de mon push bizzare a cause du truc de fusion

--- CurrentCode/R2D2/affichage/__init__.py
+++ CurrentCode/R2D2/affichage/__init__.py
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 from .affichage import Affichage
 
 __all__ = ["Affichage"]
-=======
-from .affichage import Affichage
->>>>>>> fb13e81096299a7abca6dbd4e3a5cc1d1fe96a3f
+
--- CurrentCode/R2D2/controller/__init__.py
+++ CurrentCode/R2D2/controller/__init__.py
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-from .avancer import Avancer
-from .approcher_mur import Approcher_mur
-from .boucle import Boucle
-from .carre import carre
-from .controller import controller
-from .sequencielle import Sequencielle
-
-__all__ = ["Avancer","Approcher_mur","Boucle","carre","controller","Sequencielle"]
-=======
 from .controller import Controller
 from .approcher_mur import Approcher_mur
 from .avancer import Avancer
@@ -15,4 +5,6 @@ from .boucle import Boucle
 from .carre import Carre
 from .sequencielle import Sequencielle
 from .tourner import Tourner
->>>>>>> fb13e81096299a7abca6dbd4e3a5cc1d1fe96a3f
+
+__all__ = ["Avancer","Approcher_mur","Boucle","carre","controller","Sequencielle"]
+
