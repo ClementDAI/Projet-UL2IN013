9b7f83b32119fa9ff57c07222b88096c6742b15a
ClementDAI <Clement.Dai@etu.sorbonne-universite.fr>
2026-04-01 09:11:21 +0200

    Modification des stratégies

--- CurrentCode/R2D2/traducteur/traducteurReel.py
+++ CurrentCode/R2D2/traducteur/traducteurReel.py
@@ -7,6 +7,9 @@ class TraducteurReel:
     def set_vitesse(self, vangGauche, vangDroite):
         self.robot.set_motor_dps(robot.MOTOR_LEFT, vangGauche)
         self.robot.set_motor_dps(robot.MOTOR_RIGHT, vangDroite)
+    
+    def get_capteur(self):
+        return get_distance
 
     def set_angle_zero(self):
         pos_g, pos_d = self.robot.get_motor_position()
@@ -17,3 +20,4 @@ class TraducteurReel:
         new_pos_g, new_pos_d = self.robot.get_motor_position()
         distance_roue_gauche = new_pos_g/360*(2*math.pi*self.robot.WHEEL_DIAMETER/2)
         distance_roue_droite = new_pos_d/360*(2*math.pi*self.robot.WHEEL_DIAMETER/2) #on fait set_angle_zero puis on fait avancer() sa change l angle avec ce nouvelle angle ou fait get_distance_parcourue on a angle/360= le nb de tour multiplier par le perimetre
+        return distance_roue_gauche, distance_roue_droite
