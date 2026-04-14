698fbf66c4fafa5fed2ed8932a5560d84cf66f05
tomcoipel-eng <tom.coipel@gmail.com>
2026-04-14 09:50:52 +0200

    fichier main reel pour pas tout mélanger

---
+++ CurrentCode/mainreel.py
@@ -0,0 +1,27 @@
+from R2D2.controller import Controller
+from R2D2.robot2I013 import Robot2IN013
+from R2D2.traducteur.traducteruReel import TraducteurReel
+import time 
+
+running = True
+Simu = True
+temps = 0
+
+if Simu:
+    simulation = Simulation(10,10,100,10,5,96,60)
+    affichage = Affichage(simulation)
+    traducteur = TraducteurSimu(simulation.rob)
+else:
+    robot = Robot2IN013()
+    traducteur = TraducteurReel(robot)
+controller = Controller(traducteur)
+while running:
+    deb = time.time()
+    simulation.updateSimulation(temps)
+    controller.update()
+    fin = time.time()
+    temps = fin - deb
+
+
+
+    
