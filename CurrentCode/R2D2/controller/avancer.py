d9048257d9270c7d6cb18634d28de59a4818f0a6
Martial WANG <wangmartial0@gmail.com>
2026-04-15 10:55:47 +0200

    avancer + distance parcourue modif

--- CurrentCode/R2D2/controller/avancer.py
+++ CurrentCode/R2D2/controller/avancer.py
@@ -2,16 +2,18 @@ from ..simulation.robot import Robot
 import math
 
 class Avancer:
-    def __init__(self, distance, trad):
+    def __init__(self, distance, vitesse, trad):
         self.distance = distance
         self.trad = trad
+        self.vitesse = vitesse
         
     def start(self):
         self.parcouru = 0
-        self.trad.set_vitesse(50,50)
+        self.trad.set_vitesse(self.vitesse,self.vitesse)
 
     def step(self):
-        self.trad.set_vitesse(50,50)
+        self.trad.set_angle_roue_zero()
+        self.trad.set_vitesse(self.vitesse,self.vitesse)
         self.parcouru += self.trad.get_distance_parcourue()
 
     def stop(self):
--- CurrentCode/R2D2/traducteur/traducteurReel.py
+++ CurrentCode/R2D2/traducteur/traducteurReel.py
@@ -3,6 +3,8 @@ import math
 class TraducteurReel:
     def __init__(self, robot):
         self.robot = robot #robot irl 
+        self.distance_g = 0
+        self.distance_d = 0
     
     def set_vitesse(self, vangGauche, vangDroite):
         self.robot.set_motor_dps(self.robot.MOTOR_LEFT, vangGauche)
@@ -11,16 +13,23 @@ class TraducteurReel:
     def set_vitesse_nulle(self):
         self.set_vitesse(0,0)
     
-    def set_angle_zero(self):
+    def set_angle_roue_zero(self):
         pos_g, pos_d = self.robot.get_motor_position()
         self.robot.offset_motor_encoder(self.robot.MOTOR_LEFT,pos_g)
         self.robot.offset_motor_encoder(self.robot.MOTOR_RIGHT,pos_d)
     
-    def get_distance_parcourue(self):
+    def get_distance_parcourue_roue(self):
         new_pos_g, new_pos_d = self.robot.get_motor_position()
         distance_roue_gauche = new_pos_g/360*(2*math.pi*self.robot.WHEEL_DIAMETER/2)
         distance_roue_droite = new_pos_d/360*(2*math.pi*self.robot.WHEEL_DIAMETER/2) #on fait set_angle_zero puis on fait avancer() sa change l angle avec ce nouvelle angle ou fait get_distance_parcourue on a angle/360= le nb de tour multiplier par le perimetre
-        return distance_roue_gauche, distance_roue_droite #dans le simu c'est une valeur unique
+        self.distance_g = distance_roue_gauche
+        self.distance_d = distance_roue_droite
+
+    def get_distance_parcourue(self):
+        self.get_distance_parcourue_roue()
+        if self.distance_d = self.distance_g: #On prend la vitesse d une seul roue quand sa va tout droit car on prend pas en compte les arc de cercle qu il ferrait si il avait deux distance de roue gauche et droite diff
+            return self.distance_g
+        
     
     def get_angle_parcouru(self):
         pos_g, pos_d = self.robot.get_motor_position()
