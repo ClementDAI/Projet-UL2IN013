39e2a96a803704b8eedce8073a7df4674d8d9736
Wonja <tom.coipel@gmail.com>
2026-04-24 16:22:38 +0200

    mm chose pour tourner.py + enlevé angle depart qui sert a rien

--- CurrentCode/R2D2/controller/tourner.py
+++ CurrentCode/R2D2/controller/tourner.py
@@ -3,26 +3,17 @@ from ..simulation.robot import Robot
 class Tourner:
     def __init__(self,angle,trad):
         self.angle_cible = angle
-        self.angle_depart = None
         self.trad = trad
     
     def start(self):
         self.angle_parcouru = 0
+        self.trad.set_angle_roue_zero()
         self.trad.set_vitesse(-10, 10)
 
     def step(self):
+        self.angle_parcouru += abs(self.trad.get_angle_parcouru())
         self.trad.set_angle_roue_zero()
         self.trad.set_vitesse(-10, 10)
-        self.angle_parcouru += abs(self.trad.get_angle_parcouru())
+
     def stop(self):
         return self.angle_parcouru >= self.angle_cible 
-
-
-#a tester pour le reel si ca marche mieux 
-# def start(self):
-#     self.angle_parcouru = 0
-#     self.trad.set_vitesse(-10, 10)
-
-# def step(self):
-#     self.trad.set_vitesse(-10, 10)
-#     self.angle_parcouru = abs(self.trad.get_angle_parcouru())
