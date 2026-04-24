55f7f247afe1b533a35ef477aed19264bf19f253
Wonja <tom.coipel@gmail.com>
2026-04-24 16:49:12 +0200

    nouvelle version de controller reel pour pouvoir tester chaque strat il faut juste décommenter les lignes qu'on veut tester

--- CurrentCode/R2D2/controller/controller.py
+++ CurrentCode/R2D2/controller/controller.py
@@ -3,44 +3,29 @@ from .avancer import Avancer
 from .tourner import Tourner
 from .approcher_mur import Approcher_mur
 
-#a modifier apres avoir testé que tout marche avec le traducteur simu et réel
-
 class Controller:
     def __init__(self, trad):
         self.trad = trad
-        self.strat = ""
-        self.action = [Tourner(90, self.trad)]
         self.current = -1
-    
-    def update(self): 
-        """met à jour le controller """
-        '''if self.action == None:
-            self.strat = input("Entrez une action : 0 pour Avancer, 1 pour tourner, 2 pour carré, 3 pour s'approcher d'un mur : ")
-            if self.strat == "0":
-                valeur = float(input("Distance (positif) que le robot avance : "))
-                self.action = Avancer(valeur, self.trad)
-                self.action.start()
-            
-            elif self.strat == "1":
-                valeur = float(input("Angle (positif) que le robot doit tourner : "))
-                self.action = Tourner(valeur, self.trad)
-                self.action.start()
-            
-            elif self.strat == "2":
-                valeur = float(input("Valeur (positif) du coté du carré à tracer par le robot : "))
-                self.action = Carre(valeur, self.trad)
-                self.action.start()
-            
-            elif self.strat == "3":
-                self.action = Approcher_mur(self.trad)
-                self.action.start()
-            
-            else:
-                print("Choix invalide, aucune action, veuillez entrez une action valide : 0, 1, 2, 3")
-        else:'''
+
+        #avancer de 0.5m
+        self.action = [Avancer(0.5, 10, self.trad)]
+
+        #tourner de 90°
+        # self.action = [Tourner(90, self.trad)]
+
+        # carré de 0.5m
+        # self.action = [Carre(0.5, 10, self.trad)]
+
+        #avancer jusqu'au mur
+        # self.action = [Approcher_mur(self.trad)]
+
+    def update(self):
+        """Met à jour le controller : démarre, avance, et arrête la stratégie courante."""
         if self.current < 0 and len(self.action) > 0:
             self.current = 0
             self.action[self.current].start()
+
         if self.current >= 0:
             if not self.action[self.current].stop():
                 self.action[self.current].step()
@@ -49,5 +34,3 @@ class Controller:
                 self.action[self.current].start()
             else:
                 self.trad.set_vitesse_nulle()
-
-
