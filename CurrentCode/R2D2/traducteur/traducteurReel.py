70ebbfd9d2476df4c180bc44cc24ff99ca89ef2e
Wonja <tom.coipel@gmail.com>
2026-04-24 16:05:39 +0200

    test nouvelle version trad reel

--- CurrentCode/R2D2/traducteur/traducteurReel.py
+++ CurrentCode/R2D2/traducteur/traducteurReel.py
@@ -18,20 +18,18 @@ class TraducteurReel:
         self.robot.offset_motor_encoder(self.robot.MOTOR_LEFT,pos_g)
         self.robot.offset_motor_encoder(self.robot.MOTOR_RIGHT,pos_d)
     
-    def get_distance_parcourue_roue(self):
-        new_pos_g, new_pos_d = self.robot.get_motor_position()
-        distance_roue_gauche = new_pos_g/360*(2*math.pi*self.robot.WHEEL_DIAMETER/2)
-        distance_roue_droite = new_pos_d/360*(2*math.pi*self.robot.WHEEL_DIAMETER/2) #on fait set_angle_zero puis on fait avancer() sa change l angle avec ce nouvelle angle ou fait get_distance_parcourue on a angle/360= le nb de tour multiplier par le perimetre
-        self.distance_g = distance_roue_gauche
-        self.distance_d = distance_roue_droite
+    def lire_encodeur(self):
+        pos_g, pos_d = self.robot.get_motor_position()
+        self.distance_g = pos_g/360*(math.pi*self.robot.WHEEL_DIAMETER)
+        self.distance_d = pos_d/360*(math.pi*self.robot.WHEEL_DIAMETER)
 
-    def get_distance_parcourue(self):
-        self.get_distance_parcourue_roue()#On prend la vitesse d une seul roue quand sa va tout droit car on prend pas en compte les arc de cercle qu il ferrait si il avait deux distance de roue gauche et droite diff
-        return self.distance_g
+    def get_distance_parcourue_roue(self):
+        self.lire_encodeur()
+        return (self.distance_g + self.distance_d)/2 #moyenne pas une seule roue au cas ou une roue bloque
         
     
     def get_angle_parcouru(self):
-        self.get_distance_parcourue_roue()
+        self.lire_encodeur()
         return math.degrees((self.distance_d - self.distance_g) / self.robot.WHEEL_BASE_WIDTH)
 
     
